bf1804dbeb315607c24eedef3a13287a63c63ed4
juliegodoi <juliegodoi@gmail.com>
2023-09-23 11:50:39 -0300

    adjust commit

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function toggleMode() {
   const img = document.querySelector('#profile img')
 
   if(html.classList.contains('dark')) {
-    img.setAttribute('src', './assets/avatar-dark.png')
+    img.setAttribute('src', './assets/avatar.png')
   }
   else {
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute('src', './assets/avatar-dark.png')
   }
 }
